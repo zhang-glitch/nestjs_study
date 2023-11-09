fbea5d9ac8603ae3013f02fcf82a77e5da33b4e2
zhang-glitch <669638556@qq.com>
2023-11-09 15:53:22 +0800

    feat: 通用读取环境变量逻辑实现

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
+import { UserService } from './user/user.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import * as dotEnv from 'dotenv';
 
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  // 导入模块
+  // ConfigModule.forRoot 加载环境变量
+  imports: [
+    ConfigModule.forRoot({
+      // 将环境变量在全部文件模块中导入
+      isGlobal: true,
+      // 设置读取的环境变量文件
+      envFilePath,
+      // 加载自定义配置文件数组。这里可以加载提取出的相同配置文件属性
+      load: [() => dotEnv.config({ path: '.env' })],
+    }),
+    UserModule,
+  ],
+  // 注册控制器
+  controllers: [AppController, UserController],
+  // 依赖注入，在控制器中自动实例化该服务
+  providers: [AppService, UserService],
 })
 export class AppModule {}
